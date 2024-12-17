c9dc6c38ace840e1dfe1a01830c71889ed742e79
David-bot-ui <luffymonkey1292@gmail.com>
2024-12-17 16:56:32 +0500

    Add files via upload

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,78 +1,103 @@
-import React, {useState} from 'react'
-import './Header.scss'
-import { Link } from 'react-router-dom'
-import { useAppContext } from '../../context'
+import React, { useEffect } from 'react';
+import './Header.scss';
+import { Link } from 'react-router-dom';
+import { useAppContext } from '../../context';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Header = () => {
- 
-  const{ isActive, toggleBurger,  user, isAuth, logout} = useAppContext()
-  
+  const { isActive, toggleBurger, user, isAuth, logout } = useAppContext();
 
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Длительность анимации
+      easing: 'ease-out', // Плавная анимация
+      once: true, // Анимация запускается один раз
+    });
+  }, []);
 
   return (
     <>
-    <header className="header">
+      <header className="header" data-aos="zoom-in" data-aos-delay="200">
         <div className="container">
-            <div className='header__wrapper'>
-              <div className="header__wrap">
-              <div className="header__header">
-              <Link to={'/'}><img className='logo__img' src="/shop.png" alt="" /></Link>
-                <div className={`header__menu ${isActive ? 'active' : ''}`}>
-                    <Link to={'/sale'} className='dropdown' >
+          <div className="header__wrapper">
+            <div className="header__wrap" data-aos="zoom-in" data-aos-delay="300">
+              <div className="header__header" data-aos="zoom-in" data-aos-delay="400">
+                <Link to={'/'}>
+                  <img
+                    className="logo__img"
+                    src="/shop.png"
+                    alt="Logo"
+                    data-aos="zoom-in"
+                    data-aos-delay="500"
+                  />
+                </Link>
+                <div
+                  className={`header__menu ${isActive ? 'active' : ''}`}
+                  data-aos="zoom-in"
+                  data-aos-delay="600"
+                >
+                  <Link to={'/sale'} className="dropdown" data-aos="zoom-in" data-aos-delay="700">
                     Shop
-                    <img  src="https://www.iconpacks.net/icons/2/free-arrow-down-icon-3101-thumb.png" alt="" />
-
+                    <img
+                      src="https://www.iconpacks.net/icons/2/free-arrow-down-icon-3101-thumb.png"
+                      alt="Arrow"
+                    />
                     <div>
                       <Link to={'/'}>Men</Link>
                       <Link to={'/'}>Women</Link>
                       <Link to={'/'}>Kids</Link>
-
                     </div>
-                    </Link>
-
-                    <a href="/#selling">On Sale</a>
-                    <a href="/#arrivals">New Arrivals</a>
-                    <Link to={'/brands'}>Brands</Link>
-            </div>                
+                  </Link>
+                  <a href="/#selling" data-aos="zoom-in" data-aos-delay="800">
+                    On Sale
+                  </a>
+                  <a href="/#arrivals" data-aos="zoom-in" data-aos-delay="900">
+                    New Arrivals
+                  </a>
+                  <Link to={'/brands'} data-aos="zoom-in" data-aos-delay="1000">
+                    Brands
+                  </Link>
+                </div>
               </div>
 
-            <div onClick={toggleBurger} className={`burger ${isActive ? 'active' : ''}`}>
-                <div className='div'></div>
-                <div className='div'></div>
-                <div className='div'></div>
-            </div>                
+              <div
+                onClick={toggleBurger}
+                className={`burger ${isActive ? 'active' : ''}`}
+                data-aos="zoom-in"
+                data-aos-delay="1100"
+              >
+                <div className="div"></div>
+                <div className="div"></div>
+                <div className="div"></div>
               </div>
-
-                <div className="header__search">
-                  <div className='header__input'>
-                    <img src="/search-icon.svg" alt="" />
-                   <p>Search for products...</p>
-                   </div>
-                   <Link to={'/basket'}>
-                  <img src="/icon-1.svg" alt="" />
-                   </Link>
-
-               {isAuth ? (
-                <div>
-                  <b>{user.username}</b>
-                <button onClick={logout}>logout</button>
-                </div>
-               ):(
-                <Link to={'/login'}>
-                <img src="/icon-2.svg" alt="" />
-                </Link>
-               )}
-
-
-
-                 
-                </div>
             </div>
+
+            <div className="header__search" data-aos="zoom-in" data-aos-delay="1200">
+              <div className="header__input">
+                <img src="/search-icon.svg" alt="Search" />
+                <p>Search for products...</p>
+              </div>
+              <Link to={'/basket'} data-aos="zoom-in" data-aos-delay="1300">
+                <img src="/icon-1.svg" alt="Basket" />
+              </Link>
+
+              {isAuth ? (
+                <div data-aos="zoom-in" data-aos-delay="1400">
+                  <b>{user.username}</b>
+                  <button onClick={logout}>logout</button>
+                </div>
+              ) : (
+                <Link to={'/login'} data-aos="zoom-in" data-aos-delay="1400">
+                  <img src="/icon-2.svg" alt="Login" />
+                </Link>
+              )}
+            </div>
+          </div>
         </div>
-    </header>
-
+      </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,50 +1,76 @@
-import React from 'react'
-import './Hero.scss'
+import React, { useEffect } from 'react';
+import './Hero.scss';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 const Hero = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Длительность анимации
+      easing: 'ease-in-out', // Плавная анимация
+      once: true, // Анимация запускается один раз
+    });
+  }, []);
+
   return (
     <>
-    <section className="hero">
-      <div className="container">
-        <div className="hero__wrapper">
-        <div className="hero__box">
-          <h2>FIND CLOTHES THAT MATCHES YOUR STYLE</h2>
-          <p>Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</p>
-          <button>Shop Now</button>
-          <div className="hero__costumers">
-            <div className="costumer1">
-            <h3>200+</h3>
-            <p>International Brands</p>
+      <section className="hero">
+        <div className="container">
+          <div className="hero__wrapper">
+            <div
+              className="hero__box"
+              data-aos="fade-right"
+              data-aos-delay="200"
+            >
+              <h2>FIND CLOTHES THAT MATCHES YOUR STYLE</h2>
+              <p>
+                Browse through our diverse range of meticulously crafted
+                garments, designed to bring out your individuality and cater to
+                your sense of style.
+              </p>
+              <button data-aos="zoom-in" data-aos-delay="400">Shop Now</button>
+              <div
+                className="hero__costumers"
+                data-aos="fade-up"
+                data-aos-delay="600"
+              >
+                <div className="costumer1" data-aos="fade-up" data-aos-delay="700">
+                  <h3>200+</h3>
+                  <p>International Brands</p>
+                </div>
+                <hr />
+                <div className="costumer1" data-aos="fade-up" data-aos-delay="800">
+                  <h3>2,000+</h3>
+                  <p>High-Quality Products</p>
+                </div>
+                <hr />
+                <div className="costumer1" data-aos="fade-up" data-aos-delay="900">
+                  <h3>30,000+</h3>
+                  <p>Happy Customers</p>
+                </div>
+              </div>
             </div>
-            <hr/>
-            <div className="costumer1">
-            <h3>2,000+</h3>
-            <p>High-Quality Products</p>
-            </div>
-            <hr/>
-            <div className="costumer1">
-            <h3>30,000+</h3>
-            <p>Happy Customers</p>
+            <div
+              className="hero__img"
+              data-aos="fade-left"
+              data-aos-delay="500"
+            >
+              <img src="/hero-img.png" alt="Hero" />
             </div>
           </div>
         </div>
-        <div className="hero__img">
-        <img src="/hero-img.png" alt="" />
+        <div className="logo__images" data-aos="fade-up" data-aos-delay="1000">
+          <img src="/versage.png" alt="Versage" data-aos="zoom-in" data-aos-delay="1100" />
+          <img src="/zara.png" alt="Zara" data-aos="zoom-in" data-aos-delay="1200" />
+          <img src="/gucci.png" alt="Gucci" data-aos="zoom-in" data-aos-delay="1300" />
+          <img src="/prada.png" alt="Prada" data-aos="zoom-in" data-aos-delay="1400" />
+          <img src="/calvin.png" alt="Calvin Klein" data-aos="zoom-in" data-aos-delay="1500" />
         </div>
-        </div>
-      </div>
-      <div className="logo__images">
-        <img src="/versage.png" alt="" />
-        <img src="/zara.png" alt="" />
-        <img src="/gucci.png" alt="" />
-        <img src="/prada.png" alt="" />
-        <img src="/calvin.png" alt="" />
-      </div>
-
-    </section>
-    
-    
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
+
+
